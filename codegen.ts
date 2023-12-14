80c314e0a5891a577de597c00120556940d9ded4
Matt <mail@mattcena.de>
2023-12-14 09:05:18 +0100

    extend initial setup

---
+++ codegen.ts
@@ -0,0 +1,37 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+    overwrite: true,
+    ignoreNoDocuments: true,
+    schema: [
+        {
+            [`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/mordheim-companion` || '']: {
+                headers: {
+                    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+                },
+            },
+        },
+    ],
+    generates: {
+        'src/types/contentful/contentful.schema.graphql': {
+            plugins: ['schema-ast'],
+        },
+        'src/types/contentful/contentful.ts': {
+            hooks: {
+                afterOneFileWrite: ['npx eslint --fix', 'npx prettier --write'],
+            },
+            overwrite: true,
+            documents: ['src/app/**/page.tsx', 'src/components/**/*.tsx', 'src/modules/**/*.tsx', 'src/templates/**/*.tsx'],
+            plugins: ['typescript', 'typescript-operations'],
+            config: {
+                enumsAsTypes: true,
+                onlyOperationTypes: true,
+                preResolveTypes: true,
+                useImplementingTypes: true,
+                disableDescriptions: true,
+            },
+        },
+    },
+}
+
+export default config
